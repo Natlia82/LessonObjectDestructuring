f78b5963232ee8b0e3789ddee151e8d6f593223d
Natalia <nata@host>
2021-08-12 06:50:33 +0500

    тест

---
+++ src/js/__tests__/distructurng.test.js
@@ -0,0 +1,32 @@
+//import { test } from 'media-typer';
+import { ExpectationFailed } from 'http-errors';
+import destructuring from '../destructuring';
+
+test('test1', () => {
+    const result = destructuring({
+        name: 'Лучник',
+        type: 'Bowman',
+        health: 50,
+        level: 3,
+        attack: 40,
+        defence: 10,
+        special: [{
+                id: 8,
+                name: 'Двойной выстрел',
+                icon: 'http://...',
+                description: 'Двойной выстрел наносит двойной урон'
+            },
+            {
+                id: 9,
+                name: 'Нокаутирующий удар',
+                icon: 'http://...'
+            }
+        ]
+    });
+
+    expect(result).toEqual([
+        [8, 'Двойной выстрел', 'http://...', 'Двойной выстрел наносит двойной урон'],
+        [9, 'Нокаутирующий удар', 'http://...', 'Описание недоступно']
+    ]);
+
+});
--- src/js/destructuring.js
+++ src/js/destructuring.js
@@ -1,27 +1,4 @@
-//`const character = {
-//`    name: 'Лучник',
-//`    type: 'Bowman',
-//`    health: 50,
-//`    level: 3,
-//`    attack: 40,
-//`    defence: 10,
-//`    special: [
-//`      {
-//`        id: 8,
-//`        name: 'Двойной выстрел',
-//`        icon: 'http://...',
-//`        description: 'Двойной выстрел наносит двойной урон'
-//`      }, 
-//`      {
-//`        id: 9,
-//`        name: 'Нокаутирующий удар',
-//`        icon: 'http://...'
-//`        // <- обратите внимание, описание "засекречено"
-//`      }
-//`    ]	
-//`  }
-
-function Destructuring(charact) {
+export default function Destructuring(charact) {
     const spec = charact.special;
 
     let massNew = [];
@@ -29,8 +6,6 @@ function Destructuring(charact) {
         massNew[i] = [spec[i].id, spec[i].name, spec[i].icon, ('description' in spec[i]) ? spec[i].description : 'Описание недоступно'];
     }
 
-    //console.log(massNew);
+
     return massNew;
 }
-
-// Destructuring(character);
