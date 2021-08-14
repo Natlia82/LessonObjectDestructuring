283dfa952fd916485be5670f6733a90f5e995468
Natalia <nata@host>
2021-08-14 10:52:53 +0500

    lint

--- src/js/__tests__/distructurng.test.js
+++ src/js/__tests__/distructurng.test.js
@@ -1,32 +1,31 @@
-//import { test } from 'media-typer';
-import { ExpectationFailed } from 'http-errors';
+// import { test } from 'media-typer';
+// import { ExpectationFailed } from 'http-errors';
 import destructuring from '../destructuring';
 
 test('test1', () => {
-    const result = destructuring({
-        name: 'Лучник',
-        type: 'Bowman',
-        health: 50,
-        level: 3,
-        attack: 40,
-        defence: 10,
-        special: [{
-                id: 8,
-                name: 'Двойной выстрел',
-                icon: 'http://...',
-                description: 'Двойной выстрел наносит двойной урон'
-            },
-            {
-                id: 9,
-                name: 'Нокаутирующий удар',
-                icon: 'http://...'
-            }
-        ]
-    });
+  const result = destructuring({
+    name: 'Лучник',
+    type: 'Bowman',
+    health: 50,
+    level: 3,
+    attack: 40,
+    defence: 10,
+    special: [{
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон',
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+    },
+    ],
+  });
 
-    expect(result).toEqual([
-        [8, 'Двойной выстрел', 'http://...', 'Двойной выстрел наносит двойной урон'],
-        [9, 'Нокаутирующий удар', 'http://...', 'Описание недоступно']
-    ]);
-
+  expect(result).toEqual([
+    [8, 'Двойной выстрел', 'http://...', 'Двойной выстрел наносит двойной урон'],
+    [9, 'Нокаутирующий удар', 'http://...', 'Описание недоступно'],
+  ]);
 });
--- src/js/destructuring.js
+++ src/js/destructuring.js
@@ -1,11 +1,11 @@
 export default function Destructuring(charact) {
-    const spec = charact.special;
+  const spec = charact.special;
 
-    let massNew = [];
-    for (let i = 0; i < spec.length; i++) {
-        massNew[i] = [spec[i].id, spec[i].name, spec[i].icon, ('description' in spec[i]) ? spec[i].description : 'Описание недоступно'];
-    }
+  const massNew = [];
+  for (let i = 0; i < spec.length; i += 1) {
+    massNew[i] = [spec[i].id, spec[i].name, spec[i].icon, ('description' in spec[i]) ? spec[i].description : 'Описание недоступно'];
+  }
 
 
-    return massNew;
+  return massNew;
 }
